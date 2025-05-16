c0066af4213649cb44f47c70429b51c1a8faf0fc
Greg Pontejos <greg.pontejos@crowdstrike.com>
2025-05-16 11:02:04 -0500

    Testing

--- internal/controller/falcon_container/falconcontainer_controller.go
+++ internal/controller/falcon_container/falconcontainer_controller.go
@@ -270,13 +270,18 @@ func (r *FalconContainerReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		return ctrl.Result{}, fmt.Errorf("CA bundle not present in injector TLS Secret")
 	}
 
-	if _, err = r.reconcileConfigMap(ctx, log, falconContainer); err != nil {
+	var configMap *corev1.ConfigMap
+	if configMap, err = r.reconcileConfigMap(ctx, log, falconContainer); err != nil {
+		log.Info("ConfigMap spec", "configMap", fmt.Sprintf("%v", *configMap))
+		log.Error(err, "ConfigMap creation error")
 		err = r.StatusUpdate(ctx, req, log, falconContainer, falconv1alpha1.ConditionFailed, metav1.ConditionFalse, "Reconciling", fmt.Sprintf("failed to reconcile injector ConfigMap: %v", err))
 		if err != nil {
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{}, fmt.Errorf("failed to reconcile injector ConfigMap: %v", err)
 	}
+
+	log.Info("ConfigMap spec", "configMap", fmt.Sprintf("%v", *configMap))
 
 	if _, err = r.reconcileDeployment(ctx, log, falconContainer); err != nil {
 		err = r.StatusUpdate(ctx, req, log, falconContainer, falconv1alpha1.ConditionFailed, metav1.ConditionFalse, "Reconciling", fmt.Sprintf("failed to reconcile injector Deployment: %v", err))
